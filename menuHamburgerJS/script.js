eaa5b3c1d24c72cf5dfe37beb0c0f58ec0f8ad2d
Arek eL <alechnik@gmail.com>
2019-02-05 07:29:04 +0100

    Menu Hamburger - JS
    Signed-off-by: Arek eL <alechnik@gmail.com>

--- menuHamburgerJS/script.js
+++ menuHamburgerJS/script.js
@@ -1,5 +1,11 @@
 const burger = document.querySelector('.burger');
 
+const iconBurger = document.querySelector('.fa-bars');
+const iconX = document.querySelector('.fa-times');
+const column = document.querySelector('aside');
+
 burger.addEventListener('click', function () {
-  burger.classList.toggle('active')
+  iconBurger.classList.toggle('show');
+  iconX.classList.toggle('show');
+  column.classList.toggle('show');
 })
