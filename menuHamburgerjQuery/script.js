5873eec66f40c9aa9970261bae6e424ffe4cee56
Arek eL <alechnik@gmail.com>
2019-02-07 04:48:38 +0100

    Hamburger menu in JS and jQuery  responsive text and loop 'for'
    Signed-off-by: Arek eL <alechnik@gmail.com>

--- menuHamburgerjQuery/script.js
+++ menuHamburgerjQuery/script.js
@@ -1,15 +1,33 @@
-$('.burger').on('click', function () {
-  $('.fas, aside').toggleClass('show');
-})
+// jQuery
+
+// $('.burger').on('click', function () {
+//   $('.fas, aside, .wrapper').toggleClass('show');
+// })
+
+
+//JavaScript
 
 // const burger = document.querySelector('.burger');
 
 // const iconBurger = document.querySelector('.fa-bars');
 // const iconX = document.querySelector('.fa-times');
 // const column = document.querySelector('aside');
+// const textSizing = document.querySelector('.wrapper');
 
 // burger.addEventListener('click', function () {
 //   iconBurger.classList.toggle('show');
 //   iconX.classList.toggle('show');
 //   column.classList.toggle('show');
+//   textSizing.classList.toggle('show');
 // })
+
+// JavaSriot second option
+
+const burger = document.querySelector('.burger');
+const activeElement = document.querySelectorAll('.active');
+
+burger.addEventListener('click', function () {
+  for (let i = 0; i < activeElement.length; i++) {
+    activeElement[i].classList.toggle('show');
+  }
+})
