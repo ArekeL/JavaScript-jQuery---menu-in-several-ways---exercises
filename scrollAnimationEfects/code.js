e5fd553c85f2926f010563ed586efd044e21b2fd
Arek eL <alechnik@gmail.com>
2019-02-09 17:56:34 +0100

    jQuery-animate website/slide/pop-up information - ConditionalFunction
    Signed-off-by: Arek eL <alechnik@gmail.com>

--- scrollAnimationEfects/code.js
+++ scrollAnimationEfects/code.js
@@ -9,16 +9,18 @@ $(document).on('scroll', function () {
   const $art4 = $('.art4');
   const $op1 = $('.op1');
   const $op2 = $('.op2');
+
   const art2FromTop = $art2.offset().top
   const art3FromTop = $art3.offset().top
   const art4FromTop = $art4.offset().top
   const op1FromTop = $op1.offset().top
   const op2FromTop = $op2.offset().top
+
   const art2Height = $art2.outerHeight();
   const art3Height = $art3.outerHeight();
   const art4Height = $art4.outerHeight();
-  const op1Height = $op1.outerHeight();
-  const op2Height = $op2.outerHeight();
+  const op1Height = $op1.height();
+  const op2Height = $op2.height();
   // console.log(art2Height);
 
   if (scrollValue > art2FromTop + art2Height - windowHeight) {
@@ -30,10 +32,10 @@ $(document).on('scroll', function () {
   if (scrollValue > art4FromTop + art4Height - windowHeight) {
     $art4.addClass('active');
   }
-  if (scrollValue > op1FromTop + op1Height - windowHeight - 400) {
+  if (scrollValue > op1FromTop + op1Height / 2 - windowHeight) {
     $op1.addClass('active');
   }
-  if (scrollValue > op2FromTop + op2Height - windowHeight - 400) {
+  if (scrollValue > op2FromTop + op2Height / 2 - windowHeight) {
     $op2.addClass('active');
   }
 
